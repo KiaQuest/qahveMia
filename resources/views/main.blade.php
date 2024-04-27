44dd58e73147f8be258cb7c289c50940c36cb93f
Kiyarash Khoshmood <kkh.blackcenter@yahoo.com>
2024-04-27 18:34:54 +0300

    948

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,9 +1,33 @@
-main page
+<style>
+    .container{
+        text-align: center;
+        margin: 12rem 0;
+        font-size: 9rem;
+    }
+    button{
+        font-size: 7rem;
+        border-radius: 50px;
+        padding: 5rem;
+        margin: 10rem;
+    }
+    .all{
+        margin: 10rem 0;
+    }
+</style>
+<div class="container">
+    main page
+    <div class="all" >
+        <div class="taki">
+            <a href="{{ route('login') }}">
+                <button> Admin </button>
+            </a>
+        </div>
 
-<a href="{{ route('login') }}">
-    <button> Admin </button>
-</a>
+        <div class="taki">
+            <a href="{{ route('visitSearch') }}">
+                <button> user </button>
+            </a>
+        </div>
+    </div>
 
-<a href="{{ route('visitSearch') }}">
-    <button> users </button>
-</a>
+</div>
